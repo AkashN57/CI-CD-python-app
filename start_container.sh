17164a8a77c80f3f9102f56515495ac009248e94
Akash N <114426215+AkashN57@users.noreply.github.com>
2023-11-22 09:52:33 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull akashn57/simple-python-flask-app
+docker pull akashn57/cicd-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 akashn57/simple-python-flask-app
+docker run -d -p 5000:5000 akashn57/cicd-python-app
